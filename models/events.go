3e71cb214109aa3942f8423a23bfa31cf48bfb4d
Dariely Krefta <darielykrefta@gmail.com>
2026-03-03 09:16:19 -0300

    feat:added the database and the get events and post routes

---
+++ models/events.go
@@ -0,0 +1,71 @@
+package models
+
+import (
+	"rest-api/database"
+	"time"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"` // pesquisar como formatar a nomenclatura JSON
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserId      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() error{
+	query := `INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	statement, err := database.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	result, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := database.DB.Query(query)
+	if err != nil{
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventByID(id int64) (*Event, error){
+	query := "SELECT * FROM events WHERE id = ?"
+	row := database.DB.QueryRow(query, id)
+
+	var event Event 
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil 
+}
